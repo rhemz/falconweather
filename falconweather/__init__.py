f720eb8e73e37bb0b8e3744a2620ac291986e74b
rhemz <rczeien@gmail.com>
2017-11-04 14:10:26 -0400

    sdf

--- falconweather/__init__.py
+++ falconweather/__init__.py
@@ -1,9 +1,20 @@
 import falcon
+from webargs.falconparser import parser as falcon_parser
+from marshmallow import fields
 
 
 class WindResource(object):
 
     def on_post(self, req, resp):
+        print('posted')
+        args = falcon_parser.parse(
+            params={
+                'mph': fields.Float(missing=True, required=True)
+            },
+            req=req,
+            force_all=True
+        )
+        print(args)
         pass
 
 
